e2639812043d2be56298fc8b6ce2493d59ac8704
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-06-17 20:47:55 -0300

    commit 8

--- alura_space/setup/settings.py
+++ alura_space/setup/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path, os
 from dotenv import load_dotenv
+from django.contrib.messages import constants as messages
 
 load_dotenv()
 
@@ -137,3 +138,10 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success'
+}
